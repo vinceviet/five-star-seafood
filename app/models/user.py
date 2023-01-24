6d49f1d89baf4240c1d1df375f33341b3e562d4f
vinceviet <vince.viet@gmail.com>
2023-01-24 08:40:59 -0800

    added models

--- app/models/user.py
+++ app/models/user.py
@@ -3,6 +3,13 @@ from werkzeug.security import generate_password_hash, check_password_hash
 from flask_login import UserMixin
 
 
+def add_prefix_for_prod(attr):
+    if environment == "production":
+        return f"{SCHEMA}.{attr}"
+    else:
+        return attr
+
+
 class User(db.Model, UserMixin):
     __tablename__ = 'users'
 
@@ -10,9 +17,16 @@ class User(db.Model, UserMixin):
         __table_args__ = {'schema': SCHEMA}
 
     id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(40), nullable=False, unique=True)
+    first_name = db.Column(db.String(40), nullable=False)
+    last_name = db.Column(db.String(40), nullable=False)
     email = db.Column(db.String(255), nullable=False, unique=True)
     hashed_password = db.Column(db.String(255), nullable=False)
+    phone = db.Column(db.String(15))
+    address = db.Column(db.String(40))
+    city = db.Column(db.String(40))
+    state = db.Column(db.String(40))
+    country = db.Column(db.String(40), default='United States')
+    zip_code = db.Column(db.Integer)
 
     @property
     def password(self):
@@ -28,6 +42,81 @@ class User(db.Model, UserMixin):
     def to_dict(self):
         return {
             'id': self.id,
-            'username': self.username,
+            'firstName': self.first_name,
+            'lastName': self.last_name,
             'email': self.email
+        }
+
+
+class Product(db.Model):
+    __tablename__ = 'products'
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String(55), nullable=False)
+    origin = db.Column(db.String(55), nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    category_id = db.Column(db.Integer, db.ForeignKey(
+        add_prefix_for_prod('categories.id')), nullable=False)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'origin': self.origin,
+            'price': self.price,
+            'categoryId': self.category_id
+        }
+
+
+class Order(db.Model):
+    __tablename__ = 'orders'
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    date_time = db.Column(db.String(255), nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey(
+        add_prefix_for_prod('users.id')), nullable=False)
+    cart_id = db.Column(db.Integer, db.ForeignKey(
+        add_prefix_for_prod('carts.id')), nullable=False)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'dateTime': self.date_time,
+            'userId': self.user_id,
+            'cartId': self.cart_id
+        }
+
+
+class Cart(db.Model):
+    __tablename__ = 'carts'
+
+    if environment == "production":
+        __table_args__ = {'schema': SCHEMA}
+
+    id = db.Column(db.Integer, primary_key=True)
+    user_id = db.Column(db.Integer, db.ForeignKey(
+        add_prefix_for_prod('users.id')), nullable=False)
+    product_id = db.Column(db.Integer, db.ForeignKey(
+        add_prefix_for_prod('products.id')), nullable=False)
+    product_quantity = db.Column(db.Integer, nullable=False)
+    price = db.Column(db.Float, nullable=False)
+    total_price = db.Column(db.Float, nullable=False)
+    order_id = db.Column(db.Integer, db.ForeignKey(
+        add_prefix_for_prod('orders.id')), nullable=False)
+
+    def to_dict(self):
+        return {
+            'id': self.id,
+            'userId': self.user_id,
+            'productId': self.product_id,
+            'productQuantity': self.product_quantity,
+            'price': self.price,
+            'totalPrice': self.total_price,
+            'orderId': self.order_id
         }
