61f9e3f53f6563d9a83a6737f3823b53c5780107
vinceviet <vince.viet@gmail.com>
2023-01-27 15:40:59 -0800

    cart csss

--- react-app/src/components/Cart/index.js
+++ react-app/src/components/Cart/index.js
@@ -1,5 +1,6 @@
 import React, { useEffect } from "react";
 import { useSelector, useDispatch } from "react-redux";
+import { NavLink } from 'react-router-dom'
 import { loadCartItems, addOneToCart, minusOneToCart, removeFromCart } from "../../store/cart";
 import './Cart.css';
 
@@ -31,27 +32,36 @@ export default function Cart() {
 
     if (!cartItems.length) {
         return (
-            <div>
-                <h1>Cart</h1>
-                <div>No items in cart</div>
+            <div className='empty-cart'>
+                <div>YOUR CART IS EMPTY</div>
+                <NavLink to='/' exact={true} className='empty-cart-nav-link'>SHOP OUR PRODUCTS</NavLink>
             </div>
         )
     }
 
     return (
-        <div>
-            <h1>Cart</h1>
+        <div className='cart-container'>
+            <h1 id='cart-header'>CART</h1>
+            <div className='cart-info'>
+                <span>PRODUCT</span>
+                <div className='quantity-total'>
+                    <span>QUANTITY</span>
+                    <span>TOTAL</span>
+                </div>
+            </div>
             {cartItems.map((item) =>
-                <div>
-                    <span>{item.name}</span><br />
-                    <span>{item.description}</span><br />
-                    <div>
+                <div className='cart-cards'>
+                    <div className='img-info'>
+                        <span>{item.name}</span>
+                        <span>{item.description}</span>
+                    </div>
+                    <div className='item-quantity-total'>
                         <button onClick={(e) => handleMinusItem(e, item)}>minus 1</button>
                         <span>{item.productQuantity}</span>
                         <button onClick={(e) => handleAddItem(e, item)}>plus 1</button>
                         <button onClick={(e) => handleRemoveItem(e, item)}>Remove</button>
+                        <span>${Number(item.totalItemPrice).toFixed(2)}</span>
                     </div>
-                    <span>${Number(item.totalItemPrice).toFixed(2)}</span><br />
                 </div>
             )}
             <div>Total Price: ${Number(cartItems.reduce((total, item) => total + item.totalItemPrice, 0)).toFixed(2)}</div>
