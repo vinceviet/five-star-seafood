9e180263a328ffa85a7eddf61e199faea45405a2
vinceviet <vince.viet@gmail.com>
2023-02-01 08:24:05 -0800

    added-seeds

--- app/seeds/products.py
+++ app/seeds/products.py
@@ -25,14 +25,13 @@ def seed_products():
     cochon = Category(name='meals', sub_category='cochon')
     pizza = Category(name='meals', sub_category='pizza')
     pies = Category(name='bakery',sub_category='pies')
-    flour = Category(name='bakery',sub_category='flour')
     crackers = Category(name='bakery',sub_category='crackers')
     asian = Category(name='pantry',sub_category='asian')
     coffee = Category(name='pantry',sub_category='coffee')
     grain = Category(name='pantry',sub_category='grain')
     honey = Category(name='pantry',sub_category='honey')
 
-    categories = [finfish, oysters, shellfish, caviar, poultry, beef, lamb, pork, game, fruit, vegtables, mushrooms, seaweed, milk, butter, cheese, boxes, comal, cochon, pizza, pies, flour, crackers, asian, coffee, grain, honey]
+    categories = [finfish, oysters, shellfish, caviar, poultry, beef, lamb, pork, game, fruit, vegtables, mushrooms, seaweed, milk, butter, cheese, boxes, comal, cochon, pizza, pies, crackers, asian, coffee, grain, honey]
     [db.session.add(category) for category in categories]
 
     db.session.commit()
@@ -65,15 +64,40 @@ def seed_products():
     rasberry = Product(name='Raspberry Blueberry Yogurt', description='- 5.6oz', origin="La Fermiere", price=4, category_id=milk.id)
     mango = Product(name='Mango Passionfruit Yogurt', description='- 5.6oz', origin="La Fermiere", price=4, category_id=milk.id)
 
+    cioppino = Product(name='Cioppino Kit for Two', description='', origin="Five Star Seafood", price=80, category_id=boxes.id)
+    citrus = Product(name='California Citrus Box', description='', origin="Five Star Seafood", price=49.95, category_id=boxes.id)
+    sashimi = Product(name='Grand Sushi/Sashimi Kit', description='', origin="Five Star Seafood", price=125, category_id=boxes.id)
+
+    pecan = Product(name='Bourbon Pecan Pie', description='(Frozen)', origin="Three Babes Bakeshop", price=49.90, category_id=pies.id)
+    pumpkin = Product(name='Classic Pumpkin Pie', description='(Frozen)', origin="Three Babes Bakeshop", price=49.90, category_id=pies.id)
+    applep = Product(name='Classic Apple Pie', description='(Frozen)', origin="Three Babes Bakeshop", price=49.90, category_id=pies.id)
+    choco = Product(name='Bittersweet Chocolate Pecan', description='(Frozen)', origin="Three Babes Bakeshop", price=49.90, category_id=pies.id)
+    crumble = Product(name='Blackberry Crumble Pie', description='(Frozen)', origin="Three Babes Bakeshop", price=49.90, category_id=pies.id)
+    vegan = Product(name='Vegan Blackberry Crumble Pie', description='(Frozen)', origin="Three Babes Bakeshop", price=49.90, category_id=pies.id)
+
+    dashi = Product(name='Dashi Kombu', description='- 2lb', origin="Five Star Provisions", price=42, category_id=asian.id)
+    wasabi = Product(name='Wasabi Root', description='- ea', origin="Half Moon Bay Wasabi Company", price=42, category_id=asian.id)
+    miso = Product(name='Aedan Kyoto Sweet Miso', description='- 8oz', origin="Aeden Fermented Foods", price=14, category_id=asian.id)
+    fish_sauce = Product(name='Iwashi Whiskey Barrel Aged Fish Sauce', description='- 750ml', origin="Haku", price=33.50, category_id=asian.id)
+    yuzu = Product(name='Yuzu Juice', description='- 750ml', origin="Yakami Orchards", price=42, category_id=asian.id)
+    yuzu_kosho = Product(name='Yuzu Kosho', description='- Red(2oz)', origin="Five Star Seafood", price=7.50, category_id=asian.id)
+
+
     finfish_products = [big_glory, sea_bass, halibut, kampachi, turbot, stripe_bass]
     poultry_products = [root, fogline, quail, squab, turkey, hen]
     fruit_products = [blueberries, kiwi, orange, apples, lemons, blood]
     milk_products = [straus, peach, bean, strawberry, rasberry, mango]
+    box_products = [cioppino, citrus, sashimi]
+    pie_products = [pecan, pumpkin, applep, choco, crumble, vegan]
+    asian_products = [dashi, wasabi, miso, fish_sauce, yuzu, yuzu_kosho]
 
     [db.session.add(product) for product in finfish_products]
     [db.session.add(product) for product in poultry_products]
     [db.session.add(product) for product in fruit_products]
     [db.session.add(product) for product in milk_products]
+    [db.session.add(product) for product in box_products]
+    [db.session.add(product) for product in pie_products]
+    [db.session.add(product) for product in asian_products]
 
 
     db.session.commit()
@@ -106,15 +130,39 @@ def seed_products():
     rasberry_img = ProductImage(image_url='https://cdn.shopify.com/s/files/1/0430/7065/2581/products/FS_Provisions-087_400x.jpg?v=1612978690', product_id=rasberry.id)
     mango_img = ProductImage(image_url='https://cdn.shopify.com/s/files/1/0430/7065/2581/products/FS_-208_400x.jpg?v=1623718357', product_id=mango.id)
 
+    cioppino_img = ProductImage(image_url='https://cdn.shopify.com/s/files/1/0430/7065/2581/products/FS_Cioppino-2_600x.jpg?v=1606173473', product_id=cioppino.id)
+    citrus_img = ProductImage(image_url='https://cdn.shopify.com/s/files/1/0430/7065/2581/products/CitrusBox_2a13fa25-cb75-42a0-ac5c-1bd0a09a62ec_400x.jpg?v=1614356049', product_id=citrus.id)
+    sashimi_img = ProductImage(image_url='https://cdn.shopify.com/s/files/1/0430/7065/2581/products/newsushikit_a094a99d-5554-4396-9440-0b5c03ed62a9_400x.jpg?v=1653776959', product_id=sashimi.id)
+
+    pecan_img = ProductImage(image_url='https://cdn.shopify.com/s/files/1/0430/7065/2581/products/Bourbon-Pecan-ThreeBabes_400x.jpg?v=1667820805', product_id=pecan.id)
+    pumpkin_img = ProductImage(image_url='https://cdn.shopify.com/s/files/1/0430/7065/2581/products/pumpkin-pie-threebabes_400x.jpg?v=1667821027', product_id=pumpkin.id)
+    applep_img = ProductImage(image_url='https://cdn.shopify.com/s/files/1/0430/7065/2581/products/apple-pie-threebabes_400x.jpg?v=1667821219', product_id=applep.id)
+    choco_img = ProductImage(image_url='https://cdn.shopify.com/s/files/1/0430/7065/2581/products/Chocolate-Pecan-ThreeBabes_400x.jpg?v=1667821415', product_id=choco.id)
+    crumble_img = ProductImage(image_url='https://cdn.shopify.com/s/files/1/0430/7065/2581/products/blackberry-crumble-threebabes_400x.jpg?v=1667821623', product_id=crumble.id)
+    vegan_img = ProductImage(image_url='https://cdn.shopify.com/s/files/1/0430/7065/2581/products/vegan-blackberry-threebabes_400x.jpg?v=1667821749', product_id=vegan.id)
+
+    dashi_img = ProductImage(image_url='https://cdn.shopify.com/s/files/1/0430/7065/2581/products/Kombu_1_200x.jpg?v=1612811897', product_id=dashi.id)
+    wasabi_img = ProductImage(image_url='https://cdn.shopify.com/s/files/1/0430/7065/2581/products/HMB_Wasabi_200x.jpg?v=1612832295', product_id=wasabi.id)
+    miso_img = ProductImage(image_url='https://cdn.shopify.com/s/files/1/0430/7065/2581/products/FS_provisions-118_200x.jpg?v=1612831129', product_id=miso.id)
+    fish_sauce_img = ProductImage(image_url='https://cdn.shopify.com/s/files/1/0430/7065/2581/products/FS_VG-14_50f30b1f-08be-4e5a-9ef8-6717aa11913c_200x.jpg?v=1612832838', product_id=fish_sauce.id)
+    yuzu_img = ProductImage(image_url='https://cdn.shopify.com/s/files/1/0430/7065/2581/products/MarugotoShibori_2_200x.jpg?v=1612833078', product_id=yuzu.id)
+    yuzu_kosho_img = ProductImage(image_url='https://cdn.shopify.com/s/files/1/0430/7065/2581/products/RedYuzoKosho_200x.jpg?v=1612833236', product_id=yuzu_kosho.id)
+
     finfish_images = [big_glory_img, sea_bass_img, halibut_img, kampachi_img, turbot_img, stripe_bass_img]
     poultry_images = [root_img, fogline_img, quail_img, squab_img, turkey_img, hen_img]
     fruit_images = [blueberries_img, kiwi_img, oranges_img, apples_img, lemons_img, blood_img]
     milk_images = [straus_img, peach_img, bean_img, strawberry_img, rasberry_img, mango_img]
+    box_images = [cioppino_img, citrus_img, sashimi_img]
+    pie_images = [pecan_img, pumpkin_img, applep_img,choco_img, crumble_img, vegan_img]
+    asian_images = [dashi_img, wasabi_img, miso_img, fish_sauce_img, yuzu_img, yuzu_kosho_img]
 
-    [db.session.add(image) for image in finfish_images]
+    [db.session.add(img) for img in finfish_images]
     [db.session.add(img) for img in poultry_images]
     [db.session.add(img) for img in fruit_images]
     [db.session.add(img) for img in milk_images]
+    [db.session.add(img) for img in box_images]
+    [db.session.add(img) for img in pie_images]
+    [db.session.add(img) for img in asian_images]
 
 
     db.session.commit()
