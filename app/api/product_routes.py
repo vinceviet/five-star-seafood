c6dc120c4598070c6003d71abdbecdc0b6b8ced3
vinceviet <vince.viet@gmail.com>
2023-02-22 13:44:16 -0800

    search route

--- app/api/product_routes.py
+++ app/api/product_routes.py
@@ -1,4 +1,4 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
 from app.models import Product, Category
 
 product_routes = Blueprint('products', __name__)
@@ -14,3 +14,11 @@ def get_products_by_category(category):
 def get_product_details(id):
     product = Product.query.get(id)
     return product.to_dict()
+
+@product_routes.route('/search')
+def search_product():
+    query = request.args.get('q')
+    products = Product.query.filter(Product.like(query)).all()
+    if products:
+        return {'products': [product.to_dict() for product in products]}
+    return {'message': 'No products match this search'}
