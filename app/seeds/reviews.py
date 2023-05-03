ec3b407e5d33ea6907555e27b8a9fb7862edd39c
vinceviet <vince.viet@gmail.com>
2023-05-03 12:37:15 -0700

    review seeds

--- app/seeds/reviews.py
+++ app/seeds/reviews.py
@@ -24,8 +24,11 @@ def seed_reviews():
     review20 = Review(user_id = 1, product_id = 20, review = 'yummy', stars = 5, date_time = '4/5/23')
     review21 = Review(user_id = 1, product_id = 21, review = 'yummy', stars = 5, date_time = '4/5/23')
     review22 = Review(user_id = 1, product_id = 22, review = 'yummy', stars = 5, date_time = '4/5/23')
+    review23 = Review(user_id = 1, product_id = 23, review = 'yummy', stars = 5, date_time = '4/5/23')
+    review24 = Review(user_id = 1, product_id = 24, review = 'yummy', stars = 5, date_time = '4/5/23')
+    review25 = Review(user_id = 1, product_id = 25, review = 'yummy', stars = 5, date_time = '4/5/23')
 
-    reviews = [review1, review2, review3, review4, review5, review6, review7, review8, review9, review10, review11, review12, review13, review14, review15, review16, review17, review18, review19, review20, review21, review22]
+    reviews = [review1, review2, review3, review4, review5, review6, review7, review8, review9, review10, review11, review12, review13, review14, review15, review16, review17, review18, review19, review20, review21, review22, review23, review24, review25]
 
     [db.session.add(review) for review in reviews]
 
