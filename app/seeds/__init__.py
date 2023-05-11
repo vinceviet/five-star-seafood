07e8e522a526cb7b45af01b8672997116992cfad
vinceviet <vince.viet@gmail.com>
2023-05-11 12:41:45 -0700

    undo seeds

--- app/seeds/__init__.py
+++ app/seeds/__init__.py
@@ -1,7 +1,7 @@
 from flask.cli import AppGroup
 from .users import seed_users, undo_users
 from .products import seed_products, undo_products
-from .reviews import seed_reviews, undo_reviews
+# from .reviews import seed_reviews, undo_reviews
 
 from app.models.db import db, environment, SCHEMA
 
@@ -19,11 +19,11 @@ def seed():
         # the schema name (see comment in users.py undo_users function).
         # Make sure to add all your other model's undo functions below
         undo_users()
-        undo_reviews()
+        # undo_reviews()
         undo_products()
     seed_users()
     seed_products()
-    seed_reviews()
+    # seed_reviews()
     # Add other seed functions here
 
 
@@ -31,6 +31,6 @@ def seed():
 @seed_commands.command('undo')
 def undo():
     undo_users()
-    undo_reviews()
+    # undo_reviews()
     undo_products()
     # Add other undo functions here
